9a969d89e29711c44bea05c2f8ed532209f1ee67
Victor <93841909+Vterroso@users.noreply.github.com>
2022-06-06 17:23:28 +0200

    Add files via upload

---
+++ Ejercicio_3/factorial-break.js
@@ -0,0 +1,15 @@
+
+/*- factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+una bifurcación if y una sentencia break */
+
+let i = 1;
+var factorial = 1;
+while (true){
+    factorial = factorial * i;
+    i++
+    if (i > 10){
+        break
+    }
+}
+
+console.log(factorial);
---
+++ Ejercicio_3/factorial-while.js
@@ -0,0 +1,10 @@
+
+// factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let i  = 1;
+var factorial = 1;
+while (i <= 10){
+    factorial = factorial * i;
+    i++;
+}
+console.log(factorial);
