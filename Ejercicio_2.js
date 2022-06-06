676326a3a8ea4463ff436d4e488ff4aa3c1d1e7d
Victor <93841909+Vterroso@users.noreply.github.com>
2022-06-06 11:26:15 +0200

    Add files via upload

---
+++ Ejercicio_2.js
@@ -0,0 +1,28 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+*/
+
+const usuario = [
+    "Víctor",
+    34,
+    true,
+    new Date(1987, 10, 20),
+    {
+        título : "Cómo hacer que te pasen cosas buenas",
+        autor : "Marian Rojas Estapé",
+        fecha : new Date(2018, 10, 0),
+        url : "https://www.amazon.es/C%C3%B3mo-hacer-pasen-cosas-buenas/dp/8467053305"
+    },
+];
+
