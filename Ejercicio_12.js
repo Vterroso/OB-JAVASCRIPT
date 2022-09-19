15f09caf54c6564ef149f39c288ec5f2f1ee15f5
Victor <93841909+Vterroso@users.noreply.github.com>
2022-09-19 10:52:13 +0200

    Add files via upload

---
+++ Ejercicio_12.js
@@ -0,0 +1,11 @@
+
+function Fibonacci (num){
+    const fib = [0, 1];
+
+    for (let i = 2; i < num; i++){
+        fib [i] = fib [i-1] + fib [i-2]
+    }
+    return fib
+}
+
+console.log(Fibonacci(12))
